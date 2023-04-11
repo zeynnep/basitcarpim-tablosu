bc75cb073623a63ed7e27494c61355dacc3e9c1e
zeynep <casperzynpatk@gmail.com>
2023-04-11 03:42:30 +0300

    Create çarpım tablosu.cpp

---
+++ çarpım tablosu.cpp
@@ -0,0 +1,17 @@
+/* carpým tablosu oluþturma;*/
+#include<iostream>
+using namespace std;
+int main()
+{int i,j;
+
+   for(i=1;i<=10;i++)
+   {
+     for(j= 1;j<11;j=j+1)
+     {
+       cout<<i*j<<" ";
+   }
+    cout<<"\n";
+}
+       
+    
+}
